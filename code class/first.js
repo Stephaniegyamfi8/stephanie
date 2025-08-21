5aa2507c9b60fb4b3ea18dc0a5662a6bafeef13d
Stephaniegyamfi8 <stephaniegyamfi8@gmail.com>
2025-08-21 02:32:16 -0700

    Add files via upload

---
+++ code class/first.js
@@ -0,0 +1,211 @@
+// let names="stephanie";
+
+// names='bernice';
+// console.log(names);
+// const naming='otherthings';
+// console.log=(naming);
+
+// console.log(naming);
+
+// function age(){
+//     console. log('Ali');
+// }
+// age();
+
+
+
+// function ages(age){
+//     console.log(age)
+// }
+// ages(45);
+
+
+// let a = 4
+// let b = 7
+// function agegroups(a,b){
+//     // a=77
+//     // b=50
+//     console.log(a+b)
+// }
+// agegroups(1,8);
+
+
+// function people(q,o){
+// return q+o
+// }
+// console.log(people(4,7))
+
+// const objesct ={
+//     name:"paul",
+
+//     age:"28"
+// }
+//  console.log(objesct.age)
+
+
+
+
+// const index =['orange', 'appl', 'bananna']
+
+
+// for(let i=index.length; i>=0;  i--){
+//     console.log(index[i])
+// }
+
+
+
+// let num='3'
+// const somethings={
+ 
+//     create(num){
+//      console.log(num)
+//     }
+// }
+// somethings.create(num);
+
+
+
+// var names="stephanie"; 
+//     names='faith'
+//     let age=10
+//     age=20
+    
+// console.log('the age of '+names+ ' is '+ age);
+
+// var callStephanie='stephanie';
+// function names(){
+// console.log('hi '+callStephanie+'!')
+// }
+// names();
+
+
+
+
+// function names(callStephanie){
+//     console.log(callStephanie)
+
+// }
+// names('faith')
+// let classes1=30
+// function classes(classes1){
+//      console.log(classes1)
+
+// }
+// classes('come')
+
+
+
+
+// let bucket="something";
+//     bucket="chair";
+// console.log('we have, '+ 100 +" ,"+ bucket + ' inside bucket;');
+// let everything='we have '+ 100+' ,'+bucket +' inside bucket';
+// console .log(everything);
+
+// function
+// // function all(){
+// //     console.log('abdul','bernice','destiny','stephanie');
+// // }
+// // all()
+
+
+
+// // // function all(naming="simon(naming,age,favcolor"){
+// // //     return 'rice';
+// // // }
+// // // const container=all();
+// // // console.log(container);
+
+
+// // let number=40 ;
+// // let word='hunger';
+// // function all (){
+// // return word + number;
+// // }
+// // let call=all();
+// // console.log(call);
+
+// // let num1=40;
+// // let num2=200;
+// // let num3=50;
+// // let num4=15;
+// // let num5;
+// // function all (num1,num2,num3,num4,num5=250){
+    
+// //     return num1+num2+num3+num4+num5;
+
+// // }
+
+// // let calling=all(num1,num2,num3,num4,num5);
+// // console.log (calling);
+
+
+
+// object
+// // const country={
+// // nigeria:"jos",
+// // usa:"new york",
+// // togo:"lome",
+// // japan:"tokyo",
+// // switzerland:"geneva",
+// // ghana:"accra",
+// // canada:"vancouver",
+// // } 
+// // console.log(country);
+
+
+// ARRAY
+// // const state=[]
+// // console.log(state)
+
+
+// let anything=["faith"]
+// const state=[
+//     'bernice','stephanie','abdul','daniel','jesse','jezneel','glory','jael',
+// ]
+// let goodf =state.push(['amina'])
+
+
+// const all=['bernice', 'stephanie','abdul', 'daniel', 'jesse', 'jezneel','glory','jael']
+// // console.log(all[0])
+// // console.log(all[1])
+// // console.log(all[2])
+// // console.log(all[3])
+// // console.log(all[4])
+// // console.log(all[5])
+// // console.log(all[6])
+// // console.log(all[7])
+// // all.push('faith')
+// // all.pop('andrew')
+// // console.log(all)
+
+// for(let i=0;i<all.length;i+=2){
+//     console.log(all[i])
+// }
+
+
+
+
+const all=[
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+
+]
+// all.unshift(0)
+
+// // all.pop()
+
+// console.log(all)
+// for(let i=0;i<all.length;i+=1){
+//     console.log(all[i])
+
+// }
+
+
+
+
+
---
+++ code class/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let bucket="something";
+//     bucket="chair";
+// console.log('we have, '+ 100 +" ,"+ bucket + ' inside bucket;');
+// let everything='we have '+ 100+' ,'+bucket +' inside bucket';
+// console .log(everything);
