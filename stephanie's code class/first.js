dd0c553ece2adeea8689cddd21affabc4bc44db8
Stephaniegyamfi8 <stephaniegyamfi8@gmail.com>
2025-08-26 14:50:59 -0700

    Add files via upload

---
+++ stephanie's code class/first.js
@@ -0,0 +1,159 @@
+// let names="Kinbest";
+
+
+
+// const { all } = require("express/lib/application");
+
+// names='Faith'
+// console.log(names);
+
+
+// const nameming='otherthings';
+// console.log(nameming);
+
+//  console.log(nameming);
+
+
+// function age(){
+//   console.log('Ali');
+// }
+// age();
+
+
+
+
+  //  function ages(age){
+  //   console.log(age)
+  //  }
+  //  ages(45);
+
+// function agegroups(a=2,b=7){
+//  console.log(a+b
+// }
+// agegroups(2);
+
+// let num1=40;
+// let num2 =20;
+// let num3;
+// function all(num1, num2,num3=100){
+//   return num1+num2+num3;
+
+// }
+// let calling = all(num1,num2,num3);
+// console.log(calling);
+
+// function age(){
+//   console.log("moses");
+// }
+// age
+
+// function way(a=1,b=2,c=3){
+//   console.log("a,+b+c");
+// }
+// way(1);
+
+
+
+
+
+
+// const hope="otherthings";
+// console.log(hope);
+
+
+// console.log(garbage);
+
+
+// function hope(){
+//   console.log("otherthings,garbage");
+// }
+// hope();
+
+
+
+
+// OBJECT
+// const country={
+//   nigeria:"jos",
+//   usa:"newyork",
+//   togo:"lome'",
+//   japan:"tokyo",
+//   switzerland:"genive",
+//   ghana:"accra",
+//   canada:"vancova",
+// }
+// console.log(country.switzerland);
+
+// ARRAY
+// let something=["faith"]
+// const ste=[
+//   "bernice","stephine","abdul","daniel","jesse","jezenel","glory","jael",
+// ]
+// console.log(state.push(["faith"]));
+
+
+
+
+// const state=[
+//   "bernice","stephine","abdul","daniel","jesse","jezenel","glory","jael",
+// ]
+// console.log(state.indexOf(1))
+
+
+
+// const state=[
+//   "mary","paul","mike",
+// ]
+// console.log(state.indexOf(1)));
+
+
+// const all=[
+//   'Bernice',
+//   'stephanie',
+//   'abdul',
+//   'Daniel',
+//   'Jesse',
+//   'Jezneel',
+//   'Glory',
+//   'Jael',
+// ];
+
+// for(let i=0; i<all.length; i+=2){
+//   console.log(all[i])
+// }
+
+
+
+const all=[
+  1,
+  2,
+  3,
+  4,
+  5,
+  6
+];
+
+all.shift(20);
+
+all.pop();
+console.log(all)
+for(let i=0; i<all.length; i+=1){
+  console.log(all[i])
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
